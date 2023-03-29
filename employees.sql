056020d8f1840fd5997c9b76303da41b92d0efb4
L E V I N H O <123866135+Levinho20041206@users.noreply.github.com>
2023-03-29 12:53:54 +0500

    Add files via upload

---
+++ employees.sql
@@ -0,0 +1,15 @@
+-- 1 Ex
+SET search_path = my_schema, "$user", salesordersexample;
+-- SELECT employeeid, empfirstname, empstreetaddress, empcity
+-- FROM employees
+
+-- 2 Ex
+-- SET search_path = my_schema, "$user", schoolschedulingexample;
+-- SELECT staffid, stffirstname, stfstreetaddress 
+-- FROM staff
+-- WHERE salary > (10000)
+
+-- 3 Ex
+-- SELECT *
+-- FROM employees
+-- WHERE employeeid IS NOT NULL;
